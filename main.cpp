c04198835fea841390998f3e12363f6ea79e7d19
josetor101 <josetordecilla800@gmail.com>
2023-05-16 07:04:39 -0500

    Problem B: Beautiful Bridges

--- main.cpp
+++ main.cpp
@@ -21,10 +21,10 @@ int main() {
     }
 
     // Izquierda y derecha máxima a la que podemos ir
-    vector<long long> maxDeltaRight(n, 0), maxDeltaLeft(n, 0);
+    vector<long long> maxDistDer(n, 0), maxDistIzq(n, 0);
     for (int i = 0; i < n; i++) {
-        maxDeltaRight[i] = 2 * (h - y[i]);
-        maxDeltaLeft[i] = 2 * (h - y[i]);
+        maxDistDer[i] = 2 * (h - y[i]);
+        maxDistIzq[i] = 2 * (h - y[i]);
     }
 
     //Recorremos los pares
@@ -33,46 +33,46 @@ int main() {
             if (i == j) continue;
 
             // Valores para armar el arco
-            long long dx = abs(x[i] - x[j]);
-            long long dy = h - y[j];
+            long long arcoX = abs(x[i] - x[j]);
+            long long arcoY = h - y[j];
 
             // Distancias máximas izquierda y derecha desde i
-            long long move = static_cast<long long>(2 * (dx + dy + sqrt(2 * dx * dy)) + 1e-9);
+            long long mover = static_cast<long long>(2 * (arcoX + arcoY + sqrt(2 * arcoX * arcoY)) + 1e-9);
 
             // Actualizamos el lado
             if (i < j)
-                maxDeltaRight[i] = min(maxDeltaRight[i], move);
+                maxDistDer[i] = min(maxDistDer[i], mover);
             else
-                maxDeltaLeft[i] = min(maxDeltaLeft[i], move);
+                maxDistIzq[i] = min(maxDistIzq[i], mover);
         }
     }
 
-    vector<long long> dp(n, numeric_limits<long long>::max());
-    dp[0] = 0;
+    vector<long long> distanciaPuente(n, numeric_limits<long long>::max());
+    distanciaPuente[0] = 0;
 
     // i+1 es la columna derecha a partir de la cual vamos a empezar a construir
     for (int i = 1; i < n; i++) {
         //Construimos un arco de 0 a j, luego uno solo de j a i+1
         for (int j = i - 1; j >= 0; j--) {
             //j evita que construyamos arco de j a i
-            if (x[j] + maxDeltaRight[j] < x[i]) continue;
+            if (x[j] + maxDistDer[j] < x[i]) continue;
             //Hemos llegado tan lejos que no podemos salir
-            if (x[i] - maxDeltaLeft[i] > x[j]) break;
+            if (x[i] - maxDistIzq[i] > x[j]) break;
             // No hay construccion posible
-            if (dp[j] == numeric_limits<long long>::max()) continue;
+            if (distanciaPuente[j] == numeric_limits<long long>::max()) continue;
 
             // Mejor respuesta obtenida
-            long long newval = dp[j] + a * (h - y[i]) + b * (x[i] - x[j]) * (x[i] - x[j]);
+            long long valorActualizado = distanciaPuente[j] + a * (h - y[i]) + b * (x[i] - x[j]) * (x[i] - x[j]);
 
             // Actualizamos en caso de que la anterior no fuera la menor
-            dp[i] = min(dp[i], newval);
+            distanciaPuente[i] = min(distanciaPuente[i], valorActualizado);
         }
     }
 
-    if (dp[n - 1] == numeric_limits<long long>::max())
+    if (distanciaPuente[n - 1] == numeric_limits<long long>::max())
         cout << "impossible" << endl;
     else
-        cout << dp[n - 1] + a * (h - y[0]) << endl;
+        cout << distanciaPuente[n - 1] + a * (h - y[0]) << endl;
 
     return 0;
 }
